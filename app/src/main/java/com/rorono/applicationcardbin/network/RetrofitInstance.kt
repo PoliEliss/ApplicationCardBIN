e2d8e52a259a154f3e550de8487bdd0dae3ed00f
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-01-12 14:49:22 +0300

    RetrofitInstance added

---
+++ app/src/main/java/com/rorono/applicationcardbin/network/RetrofitInstance.kt
@@ -0,0 +1,27 @@
+package com.rorono.applicationcardbin.network
+
+import com.rorono.applicationcardbin.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+    val api: CardInfoApi by lazy {
+        retrofit.create(CardInfoApi::class.java)
+    }
+}
