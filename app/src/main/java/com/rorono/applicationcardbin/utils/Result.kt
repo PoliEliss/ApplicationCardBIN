30dbc8dedc930c1b132011876830981479ae4da2
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-01-12 14:49:42 +0300

    change type cardInfo

--- app/src/main/java/com/rorono/applicationcardbin/utils/Result.kt
+++ app/src/main/java/com/rorono/applicationcardbin/utils/Result.kt
@@ -3,6 +3,6 @@ package com.rorono.applicationcardbin.utils
 import com.rorono.applicationcardbin.models.removemodels.CardInfo
 
 sealed class Result {
-    data class Success(val cardInfo: CardInfo) : Result()
+    data class Success(val cardInfo: CardInfo?) : Result()
     data class Error(val error: String) : Result()
 }
