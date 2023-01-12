a47188a046fe2a7138fce357dd03f86aad3c29e4
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-01-12 16:04:32 +0300

    fix RepositoryDataBase

--- app/src/main/java/com/rorono/applicationcardbin/repository/RepositoryDataBase.kt
+++ app/src/main/java/com/rorono/applicationcardbin/repository/RepositoryDataBase.kt
@@ -1,4 +1,25 @@
 package com.rorono.applicationcardbin.repository
 
-class RepositoryDataBase() {
+import com.rorono.applicationcardbin.database.CardBINDataBase
+import com.rorono.applicationcardbin.models.localmodels.CardBINItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class RepositoryDataBase(private val dataBase: CardBINDataBase) {
+    private val cardBinDao = dataBase.cardBINDao()
+
+    suspend fun insertCardBIN(cardBINItem: CardBINItem) {
+        withContext(Dispatchers.IO) {
+            cardBinDao.insertCardBIN(cardBINItem = cardBINItem)
+        }
+    }
+
+    suspend fun getAllCardBIN(): ArrayList<CardBINItem> {
+
+        val listCardBIN = arrayListOf<CardBINItem>()
+        withContext(Dispatchers.IO) {
+            listCardBIN.addAll(cardBinDao.getALLListCardBIN())
+        }
+        return listCardBIN
+    }
 }
