2bc37d78c3f2948255e9b1d61bccb88fa598165e
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-01-12 14:18:58 +0300

    remove models added

---
+++ app/src/main/java/com/rorono/applicationcardbin/models/removemodels/Bank.kt
@@ -0,0 +1,8 @@
+package com.rorono.applicationcardbin.models.removemodels
+
+data class Bank(
+    val city: String?,
+    val name: String?,
+    val phone: String?,
+    val url: String?
+)
---
+++ app/src/main/java/com/rorono/applicationcardbin/models/removemodels/Number.kt
@@ -0,0 +1,6 @@
+package com.rorono.applicationcardbin.models.removemodels
+
+data class Number(
+    val length: Int?,
+    val luhn: Boolean?
+)
